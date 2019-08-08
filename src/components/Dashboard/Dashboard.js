f04df22db836f9ddc93be5385837e863d69ac79d
Alon Cohen <alon@cohencodes.com>
2019-08-07 18:03:24 -0700

    refactor

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -6,22 +6,35 @@ import JobCard from '../JobCard/JobCard';
 const Dashboard = () => {
   const { jobs } = useContext(JobContext);
 
+  const jobList = jobs.map(job => {
+    return (
+      <JobCard
+        companyName={job.companyName}
+        position={job.position}
+        category={job.category}
+        dateApplied={job.dateApplied}
+        id={job._id}
+        key={job._id}
+      />
+    );
+  });
+
+  let columns = ['WISHLIST', 'APPLIED', 'INTERVIEW', 'OFFER', 'REJECTED'];
+
+  let categories = columns.map((col, idx) => {
+    return (
+      <li key={idx} className="column">
+        {col}
+      </li>
+    );
+  });
+
   return (
     <div className="dashboard">
-      <ul className="job-list">
-        {jobs.map(job => {
-          return (
-            <JobCard
-              companyName={job.companyName}
-              position={job.position}
-              category={job.category}
-              dateApplied={job.dateApplied}
-              id={job._id}
-              key={job._id}
-            />
-          );
-        })}
-      </ul>
+      <div className="categories">
+        <ul className="category-list">{categories}</ul>
+      </div>
+      <ul className="job-list">{jobList}</ul>
     </div>
   );
 };
