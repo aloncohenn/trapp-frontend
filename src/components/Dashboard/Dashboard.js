7acd68d7e4e9884e383de1b932eac85dd18e1f4c
Alon Cohen <alon@cohencodes.com>
2019-08-08 19:56:35 -0700

    fixed dashboard columns (#11)

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -6,35 +6,35 @@ import JobCard from '../JobCard/JobCard';
 const Dashboard = () => {
   const { jobs } = useContext(JobContext);
 
-  const jobList = jobs.map(job => {
-    return (
-      <JobCard
-        companyName={job.companyName}
-        position={job.position}
-        category={job.category}
-        dateApplied={job.dateApplied}
-        id={job._id}
-        key={job._id}
-      />
-    );
-  });
-
   let columns = ['WISHLIST', 'APPLIED', 'INTERVIEW', 'OFFER', 'REJECTED'];
 
-  let categories = columns.map((col, idx) => {
+  let jobList = columns.map(column => {
     return (
-      <li key={idx} className="column">
-        {col}
-      </li>
+      <ul key={column}>
+        <h1>{column}</h1>
+        {jobs
+          .filter(job => job.category.toLowerCase() === column.toLowerCase())
+          .map(job => {
+            return (
+              <JobCard
+                companyName={job.companyName}
+                position={job.position}
+                category={job.category}
+                dateApplied={job.dateApplied}
+                id={job._id}
+                key={job._id}
+              />
+            );
+          })}
+      </ul>
     );
   });
 
   return (
     <div className="dashboard">
       <div className="categories">
-        <ul className="category-list">{categories}</ul>
+        <ul className="category-list">{jobList}</ul>
       </div>
-      <ul className="job-list">{jobList}</ul>
     </div>
   );
 };
